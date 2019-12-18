9f7d25e6490a54b77951d744ada964cde8896c42
Christian <christian.campos@eigital.com>
2019-12-18 15:53:15 -0500

    Update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import Express, {Request, Response} from "express";
+import Express from "express";
+import cors from "cors";
 import * as commitHandler from "./handlers/commit";
 
 const app = Express();
+app.use(cors());
 
 app.get("/commits", commitHandler.getCommits);
 
